cbb1d484dd3859b24a0dd8f1750185bb56c638a5
JulSh <fr.schwarz@yandex.ru>
2020-03-04 19:42:14 +0300

    Third step 2.0

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
-export const greeting = () => {
-	const userName = readlineSync.question('May I have your name?');
-	console.log("Hello, `${userName}`!");
+import readlineSync from 'readline-sync';
+
+const greeting = () => {
+	var userName = readlineSync.question('May I have your name? ');
+	console.log(`Hello, ${userName}!`);
 };
+export default greeting;
